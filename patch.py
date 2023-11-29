0bcb02d5aa6d1c96904fad38bfaf2c3d86b1642e
aria1th <35677394+aria1th@users.noreply.github.com>
2023-11-29 14:28:33 +0900

    fix typo

--- patch.py
+++ patch.py
@@ -21,8 +21,8 @@ def main():
     Main entry point.
     """
     parser = argparse.ArgumentParser(description='Patch a file.')
-    parser.add_argument('--file_path', type=str, help='path to file')
-    parser.add_argument('--line_num', type=int, help='line number')
+    parser.add_argument('--patch-file', type=str, help='path to file')
+    parser.add_argument('--line-num', type=int, help='line number')
     parser.add_argument('--content', type=str, help='content')
     args = parser.parse_args()
     patch_file(args.file_path, args.line_num, args.content)
